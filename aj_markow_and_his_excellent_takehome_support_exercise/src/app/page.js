e1c1485b9792d4bfeef9997394fe3246e8358fea
AJ Markow <alexanderjmarkow@gmail.com>
2024-06-10 18:55:53 -0500

    Add links to question pages/routes on home page.

--- aj_markow_and_his_excellent_takehome_support_exercise/src/app/page.js
+++ aj_markow_and_his_excellent_takehome_support_exercise/src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Grid, GridItem,Heading} from '@chakra-ui/react'
+import { Link,Divider,Center,Grid, GridItem,Heading, OrderedList,ListItem} from '@chakra-ui/react'
 
 
 export default function Page() {
@@ -11,9 +11,41 @@ export default function Page() {
       columnGap={1}
 >
       <GridItem rowSpan={3} colSpan={1} bg='#EDFDFD'>
-            <Heading size='2xl' fontWeight='bold' textAlign='left' noOfLines={1} color='black' marginLeft='5vw' marginTop='5vh' fontSize='4vh'>
+            <Heading size='2xl' fontWeight='bold' textAlign='left' noOfLines={1} color='black' marginLeft='5vw' marginTop='5vh' fontSize='4vh' marginBottom="8vh">
         Questions
         </Heading>
+        <Center>
+          <OrderedList color='black' spacing="5vh" marginBottom="10vh" fontSize={"24px"} alignContent={"center"}>
+            <Link href="/questions/1">
+              <ListItem>Question 1</ListItem>
+            </Link>
+                          <Divider />
+            <Link href="/questions/2">
+              <ListItem>Question 2</ListItem>
+            </Link>
+                          <Divider />
+                        <Link href="/questions/3">
+              <ListItem>Question 3</ListItem>
+                            
+            </Link>
+            <Divider />
+                        <Link href="/questions/4">
+              <ListItem>Question 4</ListItem>          
+            </Link>
+            <Divider />
+                        <Link href="/questions/5">
+              <ListItem>Question 5</ListItem>
+            </Link>
+                          <Divider />
+                        <Link href="/questions/6">
+              <ListItem>Question 6</ListItem>            
+            </Link>
+            <Divider />
+                        <Link href="/questions/7">
+              <ListItem>Question 7</ListItem>
+            </Link>
+          </OrderedList>
+          </Center>
       </GridItem>
       <GridItem colSpan={4} rowSpan={1} bg='#FFF8E7'>
         <Heading size='4xl' fontWeight='bold' textAlign='left' noOfLines={1} color='black' marginLeft='5vw' marginTop='5vh' fontSize='4vh'>
