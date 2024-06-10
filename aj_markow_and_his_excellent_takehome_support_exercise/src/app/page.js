09274ae254b1187b6ecdfaff48821540299d5e83
AJ Markow <alexanderjmarkow@gmail.com>
2024-06-10 18:31:57 -0500

    Add title to home page.

--- aj_markow_and_his_excellent_takehome_support_exercise/src/app/page.js
+++ aj_markow_and_his_excellent_takehome_support_exercise/src/app/page.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
-import { Grid, GridItem } from '@chakra-ui/react'
-import {Center} from '@chakra-ui/react'
+import { Grid, GridItem,Heading} from '@chakra-ui/react'
 
 
 export default function Page() {
@@ -11,7 +10,12 @@ export default function Page() {
       templateColumns='repeat(5, 1fr)'
       columnGap={1}
 >
-  <GridItem rowSpan={3} colSpan={1} bg='#EDFDFD' />
+      <GridItem rowSpan={3} colSpan={1} bg='#EDFDFD' />
+      <GridItem colSpan={4} rowSpan={1} bg='#FFF8E7'>
+        <Heading size='4xl' fontWeight='bold' textAlign='left' noOfLines={1} color='black' marginLeft='5vw' marginTop='5vh'>
+          AJ Markow's Project for Vercel's Support Engineer Exercise
+        </Heading>
+      </GridItem>
   <GridItem colSpan={4} bg='#FFF8E7' />
   <GridItem colSpan={4} bg='#FFF8E7' />
 </Grid>
