3cb9ca5efdaaf80de2c37e88cd85b1547efe45c6
AJ Markow <alexanderjmarkow@gmail.com>
2024-06-10 18:26:12 -0500

    Add chakraui grid components to define page. Add props for color, customize colum props to make grid for structuring answers + project.

--- aj_markow_and_his_excellent_takehome_support_exercise/src/app/layout.js
+++ aj_markow_and_his_excellent_takehome_support_exercise/src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from 'next-themes'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,11 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
 
-    <ThemeProvider>
           <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-</ThemeProvider>
 
   );
 }
