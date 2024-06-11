0806f4289fcc4a0224b199379217794b93494e6a
AJ Markow <alexanderjmarkow@gmail.com>
2024-06-11 09:52:03 -0500

    Add function to fetch answer data from supabase. Add components to make answers render as text. Add back button to primary page.

--- aj_markow_and_his_excellent_takehome_support_exercise/src/app/questions/[id]/page.js
+++ aj_markow_and_his_excellent_takehome_support_exercise/src/app/questions/[id]/page.js
@@ -1,17 +1,21 @@
-import { Heading,Grid, GridItem } from '@chakra-ui/react';
-
-import { createClient } from '@supabase/supabase-js'
+import { Center,Text,Link,Heading,Grid, GridItem } from '@chakra-ui/react';
+import { createClient } from '@supabase/supabase-js';
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient('https://qcwyklypqexvdbrgoxmu.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjd3lrbHlwcWV4dmRicmdveG11Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgwNjQ0MDYsImV4cCI6MjAzMzY0MDQwNn0.Aqyuhxic2dqdfrclz0kML41c6_a8TIMMlnpKUOLVYxs')
 
 export default async function Page({ params }) {
+  async function getData() {
+    const supabase = createClient('https://qcwyklypqexvdbrgoxmu.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjd3lrbHlwcWV4dmRicmdveG11Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgwNjQ0MDYsImV4cCI6MjAzMzY0MDQwNn0.Aqyuhxic2dqdfrclz0kML41c6_a8TIMMlnpKUOLVYxs')
+    console.log(supabase);
+    console.log(params.id);
+    const res = await supabase.from('questions').select().eq('id', params.id)
+    return res
+}
+ 
 
-  const { data, error } = await supabase
-  .from('questions')
-  .select()
-    .eq('id', params.id)
   
+
+  const data = await getData()
   return (
     <div>
       <Grid
@@ -21,13 +25,30 @@ export default async function Page({ params }) {
   gap={1}
 >
         <GridItem colSpan={5} rowSpan={1} bg='#EDFDFD'>
-          <Heading size='2xl' fontWeight='bold' textAlign='left' noOfLines={1} color='black' marginLeft='5vw' marginTop='5vh' fontSize='4vh' marginBottom="8vh">
-          {data.question}
+          <Heading size='4xl' fontWeight='bold' textAlign='left' color='black' marginLeft='5vw' marginTop='5vh' fontSize='2vh' marginBottom="8vh">
+            Question {params.id} → {data.data[0].question}
           </Heading>
   </GridItem>
 
-  <GridItem colSpan={5} rowSpan={5} bg='#FFF8E7' />
-</Grid>
+        <GridItem colSpan={5} rowSpan={5} bg='#FFF8E7'>
+          <Text color='black' fontSize='24px' margin='10vw' >
+                        Answer {params.id} → {data.data[0].answer}
+
+          </Text>
+        </GridItem>
+        <GridItem colSpan={5} rowSpan={1} bg='#FFF8E7'>
+          <Center>
+          <Text color='black' as='mark' textShadow={10}>          <Link color='teal.500' href='/' fontSize={"2vh"}>
+            Return to the homepage
+          </Link></Text>
+</Center>
+      </GridItem>
+      </Grid>
+
+
+
+
+
     </div>
   )
 }
