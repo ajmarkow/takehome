0ad00c54c20eeb04a84d1d7ee6158b157fb0c73f
AJ Markow <alexanderjmarkow@gmail.com>
2024-06-12 20:25:43 -0500

    Add export of revalidate constant to ensure response from supabase api is only cached for one second. Fixes issue where requests to their api after 1st question were breaking.

--- aj_markow_and_his_excellent_takehome_support_exercise/src/app/questions/[id]/page.js
+++ aj_markow_and_his_excellent_takehome_support_exercise/src/app/questions/[id]/page.js
@@ -1,54 +1,61 @@
-import { Center,Text,Link,Heading,Grid, GridItem } from '@chakra-ui/react';
-import { createClient } from '@supabase/supabase-js';
-
+import { Center, Text, Link, Heading, Grid, GridItem } from '@chakra-ui/react'
+import { createClient } from '@supabase/supabase-js'
+export const revalidate = 1;
 // Create a single supabase client for interacting with your database
 
-export default async function Page({ params }) {
-  async function getData() {
-    const supabase = createClient('https://qcwyklypqexvdbrgoxmu.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjd3lrbHlwcWV4dmRicmdveG11Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgwNjQ0MDYsImV4cCI6MjAzMzY0MDQwNn0.Aqyuhxic2dqdfrclz0kML41c6_a8TIMMlnpKUOLVYxs')
-    console.log(supabase);
-    console.log(params.id);
+export default async function Page ({ params }) {
+  async function getData () {
+    const supabase = createClient(
+      'https://qcwyklypqexvdbrgoxmu.supabase.co',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjd3lrbHlwcWV4dmRicmdveG11Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgwNjQ0MDYsImV4cCI6MjAzMzY0MDQwNn0.Aqyuhxic2dqdfrclz0kML41c6_a8TIMMlnpKUOLVYxs'
+    )
+    console.log(supabase)
+    console.log(params.id)
     const res = await supabase.from('questions').select().eq('id', params.id)
     return res
-}
- 
-
-  
+    console.log(res);
+  }
 
   const data = await getData()
   return (
     <div>
       <Grid
-  h='100vh'
-  templateRows='repeat(6, 1fr)'
-  templateColumns='repeat(5, 1fr)'
-  gap={1}
->
+        h='100vh'
+        templateRows='repeat(6, 1fr)'
+        templateColumns='repeat(5, 1fr)'
+        gap={1}
+      >
         <GridItem colSpan={5} rowSpan={1} bg='#EDFDFD'>
-          <Heading size='4xl' fontWeight='bold' textAlign='left' color='black' marginLeft='5vw' marginTop='5vh' fontSize='2vh' marginBottom="8vh">
+          <Heading
+            size='4xl'
+            fontWeight='bold'
+            textAlign='left'
+            color='black'
+            marginLeft='5vw'
+            marginTop='5vh'
+            fontSize='2vh'
+            marginBottom='8vh'
+          >
             Question {params.id} → {data.data[0].question}
           </Heading>
-  </GridItem>
+        </GridItem>
 
         <GridItem colSpan={5} rowSpan={5} bg='#FFF8E7'>
-          <Text color='black' fontSize='24px' margin='10vw' >
-                        Answer {params.id} → {data.data[0].answer}
-
+          <Text color='black' fontSize='24px' margin='10vw'>
+            Answer {params.id} → {data.data[0].answer}
           </Text>
         </GridItem>
         <GridItem colSpan={5} rowSpan={1} bg='#FFF8E7'>
           <Center>
-          <Text color='black' as='mark' textShadow={10}>          <Link color='teal.500' href='/' fontSize={"2vh"}>
-            Return to the homepage
-          </Link></Text>
-</Center>
-      </GridItem>
+            <Text color='black' as='mark' textShadow={10}>
+              {' '}
+              <Link color='teal.500' href='/' fontSize={'2vh'}>
+                Return to the homepage
+              </Link>
+            </Text>
+          </Center>
+        </GridItem>
       </Grid>
-
-
-
-
-
     </div>
   )
 }
